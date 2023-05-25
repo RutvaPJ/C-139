7a6945ef5d46b8007c3692b4f97dbe9c32131ea0
RutvaPJ <109849306+RutvaPJ@users.noreply.github.com>
2023-05-25 20:19:05 +1000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,42 @@
+img="";
+objects=[];
+status="";
+function setup(){
+    canvas=createCanvas(640,420);
+    canvas.center();
+    video=createCapture(VIDEO);
+    video.hide();
+    objectDetector = ml5.objectDetector('cocossd',modelLoaded);
+    document.getElementById("status").innerHTML="Status: detecting object";
+}
+function modelLoaded(){
+    console.log("Model Loaded");
+    status=true;
+}
+function gotResult(error,results){
+if (error){
+    console.log('error')
+}
+else{console.log(results);
+objects=results;}
+}
+function preload(){
+    img=loadImage('dog_cat.jpg');
+}
+function draw(){
+    
+    image(video,0,0,640,420);
+    if(status != "")
+    { r=random(255);
+        g=random(255);
+        b=random(255);
+        objectDetector.detect(video,gotResult);
+        for (var i = 0; i < objects.length; i++)
+    { document.getElementById("status").innerHTML = "Status : Object Detected";
+    fill(r,g,b);
+    percent = floor(objects[i].confidence * 100);
+    text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y + 15);
+    noFill();
+    stroke(r, g, b);
+    rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height); } }
+}
